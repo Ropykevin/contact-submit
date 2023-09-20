c150ce21e960c938a44aa1c20bd20313049a4d7e
Ropy Kevin <ropykevin@gmail.com>
2023-09-19 20:58:51 -0900

    first_commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']    
+    return redirect('/thank_you.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
